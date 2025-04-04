f8b6d33b1bf893bdd2a4aecf74ee257da5aa5773
2473954 <cassie.deg.pl@gmail.com>
2025-04-04 10:02:19 +0200

    Forgotten file and update gitignore

---
+++ WebApplication1/Controllers/Frorm/GetUser_Response.cs
@@ -0,0 +1,10 @@
+﻿using WebApplication1.Database.Models;
+
+namespace WebApplication1.Controllers.Frorm;
+
+public class GetUser_Response
+{
+    public ulong Id { get; set; }
+    public string Name { get; set; }
+    public IEnumerable<GetGrade_Response> Grades { get; set; } = new List<GetGrade_Response>();
+}
